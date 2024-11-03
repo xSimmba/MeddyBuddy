76a6c17e29a46bd113b5aa665fbc27a257268b7c
LuisBBandeira <luisfilipebentobandeira@gmail.com>
2024-11-03 23:18:15 +0000

    .

--- setup/urls.py
+++ setup/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
     path("account/", include("users.urls")),
     path('medication/', include("medication.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.forms.models import model_to_dict
 
+def home(request):
+    return render(request, "home.html")
 
 def user_login(request):
     if request.method == "POST":
@@ -20,7 +22,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse("Authenticated successfully")
+                    return(redirect("home"))
             else:
                 return HttpResponse("Invalid login")
     else:
@@ -40,10 +42,10 @@ def register(request):
             new_user = form.save(commit=False)
             new_user.set_password(form.cleaned_data["password"])
             new_user.save()
-            return render(request, "registeration/register_done.html")
+            return render(request, "registration/register_done.html")
     else:
         form = UserRegistrationForm()
-    return render(request, "registeration/register.html", {"user_form": form})
+    return render(request, "registration/register.html", {"user_form": form})
 
 
 def profile(request):
